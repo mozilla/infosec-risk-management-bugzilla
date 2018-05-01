5332b2424f20b7a18f9596ac27fe16e9eb6757f8
Guillaume Destuynder <kang+mozilla@insecure.ws>
2018-05-01 12:17:42 -0700

    fix duplicate state (does not actually change anything, but it shouldnt be there)

--- casa.py
+++ casa.py
@@ -108,8 +108,7 @@ class Casa:
                          'INVALID': ('Pending', 'none'),
                          'DUPLICATE': ('Pending', 'none'),
                          'WONTFIX': ('Do not use', 'rejected'),
-                         'INCOMPLETE': ('Warning: Outstanding issues, refer to the RRA', 'done'),
-                         'FIXED': ('Completed: No outstanding issues found', 'done')
+                         'INCOMPLETE': ('Warning: Outstanding issues, refer to the RRA', 'done')
                        }
         decisionLabel, decision = decision_map.get(bug_resolution)
 
